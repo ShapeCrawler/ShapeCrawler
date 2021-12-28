8a9d3fce779f70a57793d08d911b3ae6050537f2
Adam Shakhabov <theadamo86@gmail.com>
2021-12-28 11:10:20 +0200

    ﻿namespace ShapeCrawler;

--- ShapeCrawler/Enums/AutoFitType.cs
+++ ShapeCrawler/Enums/AutoFitType.cs
@@ -1,19 +1,21 @@
-﻿namespace ShapeCrawler;
-
-public enum AutofitType
+﻿namespace ShapeCrawler
 {
-    /// <summary>
-    ///     Do not Autofit.
-    /// </summary>
-    None = 0,
+    public enum AutofitType
+    {
+        /// <summary>
+        ///     Do not Autofit.
+        /// </summary>
+        None = 0,
 
-    /// <summary>
-    ///     Shrink text on overflow.
-    /// </summary>
-    Shrink = 1,
+        /// <summary>
+        ///     Shrink text on overflow.
+        /// </summary>
+        Shrink = 1,
 
-    /// <summary>
-    ///     Resize shape to fit text.
-    /// </summary>
-    Resize = 2
+        /// <summary>
+        ///     Resize shape to fit text.
+        /// </summary>
+        Resize = 2
+    }    
 }
+
