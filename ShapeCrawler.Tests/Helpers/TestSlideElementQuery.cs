522dbb8eb934c6c907cb6e46c6c621d3d2de7157
Adam Shakhabov <theadamo86@gmail.com>
2022-09-04 16:51:18 +0200

    SC-37: refactor ColorFormat

--- ShapeCrawler.Tests/Helpers/TestSlideElementQuery.cs
+++ ShapeCrawler.Tests/Helpers/TestSlideElementQuery.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using System.Linq;
 
 namespace ShapeCrawler.Tests.Helpers
@@ -6,11 +5,12 @@ namespace ShapeCrawler.Tests.Helpers
     public class TestSlideElementQuery
     {
         public int SlideIndex { get; set; }
-        public int ShapeId { get; set; }
+        public int? ShapeId { get; set; }
         public int ParagraphIndex { get; set; }
         public int PortionIndex { get; set; }
         public IPresentation Presentation { get; set; }
-        
+        public string ShapeName { get; set; }
+
         public IParagraph GetParagraph()
         {
             var autoShape = Presentation.Slides[SlideIndex]
@@ -20,9 +20,12 @@ namespace ShapeCrawler.Tests.Helpers
 
         public IPortion GetParagraphPortion()
         {
-            var autoShape = (IAutoShape)Presentation.Slides[SlideIndex].Shapes.First(sp => sp.Id == ShapeId);
-            
-            return autoShape.TextBox.Paragraphs[ParagraphIndex].Portions[PortionIndex];
+            var shapes = this.Presentation.Slides[this.SlideIndex].Shapes;
+            var autoShape = this.ShapeId != null 
+                ? shapes.GetById<IAutoShape>(this.ShapeId.Value) 
+                : shapes.GetByName<IAutoShape>(this.ShapeName);
+
+            return autoShape.TextBox!.Paragraphs[this.ParagraphIndex].Portions[this.PortionIndex];
         }
     }
 }
