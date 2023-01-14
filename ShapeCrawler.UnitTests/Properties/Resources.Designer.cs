3f630dcffaedc74b62e77046b8760b0d3cd6ca46
Adam Shakhabov <theadamo86@gmail.com>
2023-01-14 13:14:14 +0100

    rename unit test project

--- ShapeCrawler.UnitTests/Properties/Resources.Designer.cs
+++ ShapeCrawler.UnitTests/Properties/Resources.Designer.cs
@@ -38,7 +38,7 @@ namespace ShapeCrawler.Tests.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShapeCrawler.Tests.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShapeCrawler.UnitTests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
