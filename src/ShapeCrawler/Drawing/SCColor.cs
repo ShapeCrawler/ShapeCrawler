ea7adeb8bb6eb0007dc24467b06d0f601abc9eb8
Carlos Huchim Ahumada <carlos.huchim@cfe.mx>
2023-05-09 14:27:57 -0600

    Add `SCColor` type.

---
+++ src/ShapeCrawler/Drawing/SCColor.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Drawing;
+
+namespace ShapeCrawler.Drawing;
+
+/// <summary>
+/// Color.
+/// </summary>
+public class SCColor
+{
+    /// <summary>
+    /// Gets a black color.
+    /// </summary>
+    public static readonly SCColor Black = new(0, 0, 0);
+
+    /// <summary>
+    /// Gets a transparent color.
+    /// </summary>
+    public static readonly SCColor Transparent = new(0, 0, 0, 0);
+
+    /// <summary>
+    /// Gets a white color.
+    /// </summary>
+    public static readonly SCColor White = new(255, 255, 255);
+    /// <summary>
+    /// Set alpha value.
+    /// </summary>
+    /// <remarks>
+    /// If the alpha or opacity parameter is set t 0 it will be transparent.
+    /// </remarks>
+    private readonly float alpha;
+
+    /// <summary>
+    /// Set color blue.
+    /// </summary>
+    private readonly int blue;
+
+    /// <summary>
+    /// Set color green.
+    /// </summary>
+    private readonly int green;
+
+    /// <summary>
+    /// Set color red.
+    /// </summary>
+    private readonly int red;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SCColor"/> class.
+    /// </summary>
+    /// <param name="color">Color value.</param>
+    public SCColor(Color color)
+        : this(Convert.ToInt32(color.R), Convert.ToInt32(color.G), Convert.ToInt32(color.B), Convert.ToInt32(color.A))
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SCColor"/> class.
+    /// </summary>
+    /// <param name="red">Red value.</param>
+    /// <param name="green">Green value.</param>
+    /// <param name="blue">Blue value.</param>
+    public SCColor(int red, int green, int blue)
+        : this(red, green, blue, 255)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SCColor"/> class.
+    /// </summary>
+    /// <param name="red">Red value.</param>
+    /// <param name="green">Green value.</param>
+    /// <param name="blue">Blue value.</param>
+    /// <param name="alpha">Alpha value.</param>
+    public SCColor(int red, int green, int blue, float alpha)
+    {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+    }
+
+    /// <summary>
+    /// Gets the alpha value.
+    /// </summary>
+    /// <remarks>
+    /// Values are 0 to 255, where 0 is totally transparent.
+    /// </remarks>
+    public float Alpha => this.alpha;
+
+    /// <summary>
+    /// Gets the blue value.
+    /// </summary>
+    public int B => this.blue;
+
+    /// <summary>
+    /// Gets the green value.
+    /// </summary>
+    public int G => this.green;
+
+    /// <summary>
+    /// Gets the red value.
+    /// </summary>
+    public int R => this.red;
+
+    /// <summary>
+    /// Gets a value indicating whether if color is transparent.
+    /// </summary>
+    internal bool IsTransparent => this.Alpha == 0;
+
+    /// <summary>
+    /// Gets a value indicating whether if color is solid.
+    /// </summary>
+    internal bool IsSolid => this.Alpha == 255;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="SCColor"/> class.
+    /// </summary>
+    /// <param name="name">Name of color.</param>
+    /// <returns>A new color instance.</returns>
+    public static SCColor FromName(string name)
+    {
+        // see https://learn.microsoft.com/en-us/dotnet/api/system.drawing.color.fromname?view=net-7.0#system-drawing-color-fromname(system-string)
+        return new(Color.FromName(name));
+    }
+
+    /// <summary>
+    /// Returns a value indicating wheather hex is a valid value.
+    /// </summary>
+    /// <param name="hex">Hex value.</param>
+    /// <param name="result">Color value.</param>
+    /// <returns>Returns <see langword="true" /> if hex is a valid value. </returns>
+    public static bool TryGetColorFromHex(string hex, out SCColor result)
+    {
+        result = SCColor.Black;
+
+        try
+        {
+            // We can try to parse:
+            // 3 or 6 chars without alpha (rgb): F01, FF0011,
+            // 4 or 8 chars with alpha (rgba): F01F, FF0011FF 
+            // Ignores hex values starting with "#" character.
+            result = ParseHexValue(hex.StartsWith("#", StringComparison.Ordinal) ? hex[1..] : hex);
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        // String representation ignores alpha value.
+        return $"{this.R:X}{this.G:X}{this.B:X}";
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="SCColor"/> class.
+    /// </summary>
+    /// <param name="hex">Hex value.</param>
+    /// <returns>A new color.</returns>
+    private static SCColor ParseHexValue(string hex)
+    {
+        int a = 255;
+        int r;
+        int b;
+        int g;
+
+        switch (hex.Length)
+        {
+            // FFFF 
+            case 4:
+                a = 17 * HexValue(hex[3]);
+                goto case 3;
+            case 3:
+                // F00
+                r = 17 * HexValue(hex[0]);
+                g = 17 * HexValue(hex[1]);
+                b = 17 * HexValue(hex[2]);
+                break;
+            case 8:
+                // FFFFFF00
+                a = (16 * HexValue(hex[6])) + HexValue(hex[7]);
+                goto case 6;
+            case 6:
+                r = (16 * HexValue(hex[0])) + HexValue(hex[1]);
+                g = (16 * HexValue(hex[2])) + HexValue(hex[3]);
+                b = (16 * HexValue(hex[4])) + HexValue(hex[5]);
+                break;
+            default:
+                // String format is invalid.
+                throw new FormatException("Hex value is invalid");
+        }
+
+        static int HexValue(char hex)
+        {
+            return Convert.ToInt32($"0x{hex}", 16);
+        }
+
+        return new(r, g, b, a);
+    }
+}
---
+++ test/ShapeCrawler.Tests.Unit/ColorTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using ShapeCrawler.Drawing;
+using ShapeCrawler.Tests.Unit.Helpers;
+using System.Collections.Generic;
+using System.Drawing;
+using Xunit;
+
+namespace ShapeCrawler.Tests.Unit;
+
+public class ColorTests : SCTest
+{
+    [Fact]
+    public void Color_IsWhite()
+    {
+        var color = SCColor.White;
+
+        color.R.Should().Be(255);
+        color.G.Should().Be(255);
+        color.B.Should().Be(255);
+        color.Alpha.Should().Be(255);
+    }
+
+    [Theory]
+    [MemberData(nameof(HexValuesData))]
+    public void Color_Parse_from_hex_values(string hexValue, SCColor expected)
+    {
+        var rColor = SCColor.TryGetColorFromHex(hexValue, out var color);
+
+        rColor.Should().BeTrue();
+        color.R.Should().Be(expected.R);
+        color.G.Should().Be(expected.G);
+        color.B.Should().Be(expected.B);
+        color.Alpha.Should().Be(expected.Alpha);
+    }
+
+    [Theory]
+    [MemberData(nameof(ColorNamesData))]
+    public void Color_Parse_from_names(string name, SCColor expected)
+    {
+        var color = SCColor.FromName(name);
+
+        color.R.Should().Be(expected.R);
+        color.G.Should().Be(expected.G);
+        color.B.Should().Be(expected.B);
+        color.Alpha.Should().Be(expected.Alpha);
+    }
+
+    public static IEnumerable<object[]> HexValuesData => 
+        new List<object[]>
+        {
+            new object[] { "FFF", SCColor.White},
+            new object[] { "FFFFFF", SCColor.White},
+            new object[] { "FFFFFFFF", SCColor.White},
+            new object[] { "0000", SCColor.Transparent},
+            new object[] { "00000000", SCColor.Transparent},
+            // MS documents colors as ARGB, so we need RGBA and change alpha position.
+            // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.color.red?view=net-7.0#system-drawing-color-red
+            new object[] { "FF0000FF", new SCColor(Color.Red)},
+            // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.color.green?view=net-7.0#system-drawing-color-green
+            new object[] { "008000FF", new SCColor(Color.Green)},
+        };
+
+    public static IEnumerable<object[]> ColorNamesData =>
+        new List<object[]>
+        {
+            // MS documents colors as ARGB, so we need RGBA and change alpha position.
+            // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.color.red?view=net-7.0#system-drawing-color-red
+            new object[] { Color.Red.Name, new SCColor(Color.Red)},
+            // https://learn.microsoft.com/en-us/dotnet/api/system.drawing.color.green?view=net-7.0#system-drawing-color-green
+            new object[] { Color.Silver.Name, new SCColor(Color.Silver)},
+        };
+}
