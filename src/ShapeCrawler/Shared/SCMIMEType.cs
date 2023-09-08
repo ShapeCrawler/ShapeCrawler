bc74e2581dd2b04dbf28f63e2321dab8ecdc6790
Adam Shakhabov <theadamo86@gmail.com>
2023-09-08 19:25:47 +0200

    add summary

--- src/ShapeCrawler/Shared/SCMIMEType.cs
+++ src/ShapeCrawler/Shared/SCMIMEType.cs
@@ -1,7 +1,17 @@
 ﻿namespace ShapeCrawler.Shared;
 
+/// <summary>
+///     Represents MIME type.
+/// </summary>
 public enum SCMIMEType
 {
+    /// <summary>
+    ///    MP3 audio.
+    /// </summary>
     MP3,
+    
+    /// <summary>
+    ///     WAVE audio.
+    /// </summary>
     WAVE
 }
