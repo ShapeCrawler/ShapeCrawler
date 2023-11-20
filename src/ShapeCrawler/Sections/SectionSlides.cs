bd9ef54bc0cac2a4cdf50ffe6bae4c3db9da3260
Adam Shakhabov <theadamo86@gmail.com>
2023-11-20 19:58:11 +0100

    fix nullable

--- src/ShapeCrawler/Sections/SectionSlides.cs
+++ src/ShapeCrawler/Sections/SectionSlides.cs
@@ -32,10 +32,11 @@ internal sealed class SectionSlides : IReadOnlyList<ISlide>
     private ReadOnlySlides ReadOnlySlides()
     {
         var sdkSlideParts = new List<SlidePart>();
-        var idToRId = this.sdkPresDocument.PresentationPart!.Presentation.SlideIdList!.ChildElements.OfType<P.SlideId>().ToDictionary(x => x.Id, x => x.RelationshipId);
+        var idToRId = this.sdkPresDocument.PresentationPart!.Presentation.SlideIdList!.ChildElements.OfType<P.SlideId>()
+            .ToDictionary(x => x.Id!, x => x.RelationshipId);
         foreach (var p14SectionSlideIdListEntry in this.p14SectionSlideIdListEntryList)
         {
-            var rId = idToRId[p14SectionSlideIdListEntry.Id]!.Value!;
+            var rId = idToRId[p14SectionSlideIdListEntry.Id!]!.Value!;
             var slidePart = (SlidePart)this.sdkPresDocument.PresentationPart!.GetPartById(rId);
             sdkSlideParts.Add(slidePart);
         }
