2783ad313b268170a2d9e4a7b03a2f03a156f74c
Adam Shakhabov <theadamo86@gmail.com>
2023-12-07 21:02:49 +0100

    Remove unused using directive in Slide.cs
    The using directive for System.Threading.Tasks was removed from Slide.cs as it was not used anywhere within the file. This helps to keep the codebase clean and free of unused dependencies.

--- src/ShapeCrawler/Presentations/Slide.cs
+++ src/ShapeCrawler/Presentations/Slide.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Presentation;
