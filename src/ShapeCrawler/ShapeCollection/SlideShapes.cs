58bd8d411297ff258594657c62fffe32a7b3a826
blockingHD <blockinghd@gmail.com>
2025-01-15 23:07:39 +0000

    Reset raster stream position

--- src/ShapeCrawler/ShapeCollection/SlideShapes.cs
+++ src/ShapeCrawler/ShapeCollection/SlideShapes.cs
@@ -160,6 +160,7 @@ internal sealed class SlideShapes : ISlideShapes
             using var rasterStream = new MemoryStream();
             imageMagick.Write(rasterStream, new PngWriteDefines() { IncludeChunks = PngChunkFlags.None });
             image.Position = 0;
+            rasterStream.Position = 0;
             var pPicture = VectorImageFormats.Contains(originalFormat) 
                 ? this.CreateSvgPPicture(rasterStream, image, "Picture") 
                 : this.CreatePPicture(rasterStream, "Picture");
