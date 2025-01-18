58b4ecdc6d033d80d660758d2acab10479035977
Adam Shakhabov <theadamo86@gmail.com>
2025-01-18 15:45:33 +0100

    Remove density setting

--- src/ShapeCrawler/ShapeCollection/SlideShapes.cs
+++ src/ShapeCrawler/ShapeCollection/SlideShapes.cs
@@ -169,7 +169,6 @@ internal sealed class SlideShapes : ISlideShapes
             }
             
             var rasterStream = new MemoryStream();
-            imageMagick.Density = new Density(384, 384, DensityUnit.PixelsPerInch);
             imageMagick.Write(rasterStream, new PngWriteDefines() { IncludeChunks = PngChunkFlags.None });
             image.Position = 0;
             rasterStream.Position = 0;
