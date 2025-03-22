883eb3d3f15e5a026ac6d66042fad1ba1b1845c1
FabrisTB <fabricebalan12@gmail.com>
2025-03-22 01:11:53 +0100

    correct wrong cast in point

--- src/ShapeCrawler/Units/Points.cs
+++ src/ShapeCrawler/Units/Points.cs
@@ -2,9 +2,9 @@
 
 internal readonly ref struct Points(decimal points)
 {
-    internal long AsEmus() => (long)points * 12700;
+    internal long AsEmus() => (long)(points * 12700);
 
     internal float AsPixels() => (float)points * 96 / 72;
 
-    internal int AsHundredPoints() => (int)points * 100;
+    internal int AsHundredPoints() => (int)(points * 100);
 }
