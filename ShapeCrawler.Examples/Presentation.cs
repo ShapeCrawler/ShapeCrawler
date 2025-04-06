56533a5cd3831c8d77f146a7674ee6defec7b6c0
Adam Shakhabov <theadamo86@gmail.com>
2025-04-06 13:49:35 +0200

    Get presentation markdown

---
+++ ShapeCrawler.Examples/Presentation.cs
@@ -0,0 +1,13 @@
+﻿namespace ShapeCrawler.Examples;
+
+public class Presentation
+{
+    [Test]
+    [Explicit]
+    public void Get_markdown()
+    {
+        var pres = new ShapeCrawler.Presentation("pres.pptx");
+
+        var presMarkdown = pres.AsMarkdown();
+    }
+}
