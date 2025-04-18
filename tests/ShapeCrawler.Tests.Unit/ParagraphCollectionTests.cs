0a985c9da48c139c0bd6b900ff42dc5c10cad8dc
Adam Shakhabov <theadamo86@gmail.com>
2025-04-18 02:57:49 +0200

    Enhance ParagraphCollectionTests by adding test for inserting paragraph at the beginning

--- tests/ShapeCrawler.Tests.Unit/ParagraphCollectionTests.cs
+++ tests/ShapeCrawler.Tests.Unit/ParagraphCollectionTests.cs
@@ -6,7 +6,7 @@ namespace ShapeCrawler.Tests.Unit;
 public class ParagraphCollectionTests
 {
     [Test]
-    public void Add_adds_paragraph()
+    public void Add_adds_paragraph_in_the_middle()
     {
         // Arrange
         var pres = new Presentation();
@@ -23,6 +23,25 @@ public class ParagraphCollectionTests
 
         // Assert
         addedShape.TextBox.Text.Should().Be($"Paragraph 1{Environment.NewLine}New Paragraph 2{Environment.NewLine}Paragraph 2");
+        pres.Validate();
+    }
+    
+    [Test]
+    public void Add_adds_paragraph_at_the_beginning()
+    {
+        // Arrange
+        var pres = new Presentation();
+        var slide = pres.Slide(1);
+        slide.Shapes.AddShape(100, 100, 200, 200);
+        var addedShape = slide.Shapes.Last();
+        var paragraphs = addedShape.TextBox!.Paragraphs;
+        paragraphs[0].Text = "Paragraph 1";
+
+        // Act
+        paragraphs.Add("New Paragraph 1", 0);
+
+        // Assert
+        addedShape.TextBox.Text.Should().Be($"New Paragraph 1{Environment.NewLine}Paragraph 1");
         pres.Validate();
     }
 }
