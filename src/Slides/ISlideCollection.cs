a43f31ee8bd2a331c717b55feb489966c356ef4f
Adam Shakhabov <theadamo86@gmail.com>
2025-08-21 12:43:48 +0200

    Add functionality to insert slides at specified positions and update related methods

--- src/Slides/ISlideCollection.cs
+++ src/Slides/ISlideCollection.cs
@@ -16,6 +16,13 @@ public interface ISlideCollection : IReadOnlyList<ISlide>
     ///     Adds a new slide using specified layout.
     /// </summary>
     void Add(int layoutNumber);
+    
+    /// <summary>
+    ///     Adds a new slide using specified layout at the specified position.
+    /// </summary>
+    /// <param name="layoutNumber">Slide Layout number.</param>
+    /// <param name="slideNumber">Slide number.</param>
+    void Add(int layoutNumber, int slideNumber);
 
     /// <summary>
     ///     Adds slide.
@@ -23,7 +30,7 @@ public interface ISlideCollection : IReadOnlyList<ISlide>
     void Add(ISlide slide);
 
     /// <summary>
-    ///     Adds slide at the specified number position.
+    ///     Adds slide at the specified position.
     /// </summary>
-    void Add(ISlide slide, int number);
+    void Add(ISlide slide, int slideNumber);
 }
