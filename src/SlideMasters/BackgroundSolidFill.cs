f66903f834b12dbfe919af26732594a02d8de208
Adam Shakhabov <theadamo86@gmail.com>
2025-11-02 09:42:54 +0100

    Add BackgroundSolidFill class and refactor SlideLayoutBackground for solid fill handling

---
+++ src/SlideMasters/BackgroundSolidFill.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+
+namespace ShapeCrawler;
+
+internal sealed class BackgroundSolidFill : ISolidFill
+{
+    private readonly SlideLayoutPart slideLayoutPart;
+
+    internal BackgroundSolidFill(SlideLayoutPart slideLayoutPart)
+    {
+        this.slideLayoutPart = slideLayoutPart;
+    }
+
+    public string Color
+    {
+        get
+        {
+            var pCommonSlideData = this.slideLayoutPart.SlideLayout.CommonSlideData;
+            var pBackground = pCommonSlideData?.GetFirstChild<DocumentFormat.OpenXml.Presentation.Background>();
+            var pBackgroundProperties = pBackground?.GetFirstChild<DocumentFormat.OpenXml.Presentation.BackgroundProperties>();
+
+            if (pBackgroundProperties != null)
+            {
+                var aSolidFill = pBackgroundProperties.GetFirstChild<DocumentFormat.OpenXml.Drawing.SolidFill>();
+                if (aSolidFill != null)
+                {
+                    var aRgbColorModelHex = aSolidFill.RgbColorModelHex;
+                    if (aRgbColorModelHex != null)
+                    {
+                        return aRgbColorModelHex.Val!.ToString()!;
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
--- src/SlideMasters/ISlideLayoutBackground.cs
+++ src/SlideMasters/ISlideLayoutBackground.cs
@@ -1,6 +1,5 @@
 using DocumentFormat.OpenXml.Packaging;
 using ShapeCrawler.Extensions;
-using A = DocumentFormat.OpenXml.Drawing;
 using P = DocumentFormat.OpenXml.Presentation;
 
 #pragma warning disable IDE0130
@@ -23,75 +22,23 @@ public interface ISlideLayoutBackground
     void SolidFillColor(string hex);
 }
 
-/// <summary>
-///     Represents solid fill properties.
-/// </summary>
-public interface ISolidFill
+internal sealed class SlideLayoutBackground(SlideLayoutPart slideLayoutPart) : ISlideLayoutBackground
 {
-    /// <summary>
-    ///     Gets the color in hexadecimal format.
-    /// </summary>
-    string Color { get; }
-}
-
-internal sealed class SlideLayoutBackground : ISlideLayoutBackground
-{
-    private readonly SlideLayoutPart slideLayoutPart;
     private BackgroundSolidFill? solidFill;
 
-    internal SlideLayoutBackground(SlideLayoutPart slideLayoutPart)
-    {
-        this.slideLayoutPart = slideLayoutPart;
-    }
-
-    public ISolidFill SolidFill => this.solidFill ??= new BackgroundSolidFill(this.slideLayoutPart);
+    public ISolidFill SolidFill => this.solidFill ??= new BackgroundSolidFill(slideLayoutPart);
 
     public void SolidFillColor(string hex)
     {
-        var pCommonSlideData = this.slideLayoutPart.SlideLayout.CommonSlideData
-                               ?? this.slideLayoutPart.SlideLayout.AppendChild<P.CommonSlideData>(new());
+        var pCommonSlideData = slideLayoutPart.SlideLayout.CommonSlideData
+                               ?? slideLayoutPart.SlideLayout.AppendChild(new P.CommonSlideData());
 
         var pBackground = pCommonSlideData.GetFirstChild<P.Background>()
-                          ?? pCommonSlideData.InsertAt<P.Background>(new(), 0);
+                          ?? pCommonSlideData.InsertAt(new P.Background(), 0);
 
         var pBackgroundProperties = pBackground.GetFirstChild<P.BackgroundProperties>()
-                                    ?? pBackground.AppendChild<P.BackgroundProperties>(new());
+                                    ?? pBackground.AppendChild(new P.BackgroundProperties());
 
         pBackgroundProperties.AddSolidFill(hex);
-    }
-}
-
-internal sealed class BackgroundSolidFill : ISolidFill
-{
-    private readonly SlideLayoutPart slideLayoutPart;
-
-    internal BackgroundSolidFill(SlideLayoutPart slideLayoutPart)
-    {
-        this.slideLayoutPart = slideLayoutPart;
-    }
-
-    public string Color
-    {
-        get
-        {
-            var pCommonSlideData = this.slideLayoutPart.SlideLayout.CommonSlideData;
-            var pBackground = pCommonSlideData?.GetFirstChild<P.Background>();
-            var pBackgroundProperties = pBackground?.GetFirstChild<P.BackgroundProperties>();
-
-            if (pBackgroundProperties != null)
-            {
-                var aSolidFill = pBackgroundProperties.GetFirstChild<A.SolidFill>();
-                if (aSolidFill != null)
-                {
-                    var aRgbColorModelHex = aSolidFill.RgbColorModelHex;
-                    if (aRgbColorModelHex != null)
-                    {
-                        return aRgbColorModelHex.Val!.ToString()!;
-                    }
-                }
-            }
-
-            return string.Empty;
-        }
     }
 }
