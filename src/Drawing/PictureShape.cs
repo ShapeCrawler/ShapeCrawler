08dbad8e7e61795b1819b33c308c2867f78cdc49
Adam Shakhabov <theadamo86@gmail.com>
2025-12-14 20:51:05 +0100

    Refactor PictureShape to add CopyTo method for improved functionality

--- src/Drawing/PictureShape.cs
+++ src/Drawing/PictureShape.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using ShapeCrawler.Positions;
 using ShapeCrawler.Shapes;
 using ShapeCrawler.Units;
@@ -11,6 +11,8 @@ internal class PictureShape(Picture picture, P.Picture pPicture) : Shape(new Pos
     new ShapeSize(pPicture), new ShapeId(pPicture), pPicture)
 {
     public override IPicture Picture => picture;
+
+    public override void CopyTo(P.ShapeTree pShapeTree) => picture.CopyTo(pShapeTree);
 
     internal override void Render(SKCanvas canvas)
     {
