10f5ee4af5e8e4a64c002c100c786f2d71b171d4
Adam Shakhabov <theadamo86@gmail.com>
2025-12-28 22:03:17 +0100

    Add ShapeFillTests to validate color replacement functionality

---
+++ tests/ShapeCrawler.CITests/ShapeFillTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using ShapeCrawler.DevTests.Helpers;
+
+namespace ShapeCrawler.CITests;
+
+public class ShapeFillTests : SCTest
+{
+    [Test]
+    [TestCase("009_table.pptx", 2, "AutoShape 2")]
+    public void SetColor_replaces_picture_with_solid_color(string file, int slideNumber, string shapeName)
+    {
+        // Arrange
+        var pres = new Presentation(TestAsset(file));
+        var shape = pres.Slide(slideNumber).Shapes.Shape(shapeName);
+        var shapeFill = shape.Fill;
+        var image = TestAsset("09 png image.png");
+        var greenColor = "32a852";
+
+        // Act
+        shapeFill.SetPicture(image);
+        shapeFill.SetColor(greenColor);
+
+        // Assert
+        shapeFill.Color.Should().Be(greenColor);
+        ValidatePresentation(pres);
+    }
+}
--- tests/ShapeCrawler.DevTests/ShapeFillTests.cs
+++ tests/ShapeCrawler.DevTests/ShapeFillTests.cs
@@ -170,27 +170,7 @@ public class ShapeFillTests : SCTest
         shapeFill.Color.Should().Be("32a852");
         ValidatePresentation(pres);
     }
-
-    [Test]
-    [TestCase("009_table.pptx", 2, "AutoShape 2")]
-    public void SetColor_replaces_picture_with_solid_color(string file, int slideNumber, string shapeName)
-    {
-        // Arrange
-        var pres = new Presentation(TestAsset(file));
-        var shape = pres.Slide(slideNumber).Shapes.Shape(shapeName);
-        var shapeFill = shape.Fill;
-        var image = TestAsset("09 png image.png");
-        var greenColor = "32a852";
-
-        // Act
-        shapeFill.SetPicture(image);
-        shapeFill.SetColor(greenColor);
-
-        // Assert
-        shapeFill.Color.Should().Be(greenColor);
-        ValidatePresentation(pres);
-    }
-
+    
     [Test]
     public void SetPicture_should_not_break_presentation()
     {
