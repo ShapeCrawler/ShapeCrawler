3b1275efdd55a3edf43d2a776c68eebeb98ce22d
Adam Shakhabov <theadamo86@gmail.com>
2026-01-01 17:33:33 +0100

    Refactor UserSlide to abstract methods and introduce DrawingSlide implementation

---
+++ src/Slides/DrawingSlide.cs
@@ -0,0 +1,78 @@
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+using ShapeCrawler.Units;
+using SkiaSharp;
+
+namespace ShapeCrawler.Slides;
+
+/// <inheritdoc/>
+internal sealed class DrawingSlide(ILayoutSlide layoutSlide, UserSlideShapeCollection shapes, SlidePart slidePart)
+    : UserSlide(layoutSlide, shapes, slidePart)
+{
+    /// <inheritdoc/>
+    public override void SaveImageTo(string file)
+    {
+        using var fileStream = File.Create(file);
+        this.SaveImageTo(fileStream);
+    }
+
+    /// <inheritdoc/>
+    public override void SaveImageTo(Stream stream)
+    {
+        var presPart = this.GetSdkPresentationPart();
+        var pSlideSize = presPart.Presentation.SlideSize!;
+        var width = new Emus(pSlideSize.Cx!.Value).AsPixels();
+        var height = new Emus(pSlideSize.Cy!.Value).AsPixels();
+
+        using var surface = SKSurface.Create(new SKImageInfo((int)width, (int)height));
+        var canvas = surface.Canvas;
+
+        this.RenderBackground(canvas);
+        ((UserSlideShapeCollection)this.Shapes).Render(canvas);
+
+        using var image = surface.Snapshot();
+        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+        data.SaveTo(stream);
+
+        if (stream.CanSeek)
+        {
+            stream.Position = 0;
+        }
+    }
+
+    private SKColor GetSkColor()
+    {
+        var hex = this.Fill.Color!.TrimStart('#');
+
+        // Validate hex length before parsing.
+        if (hex.Length != 6 && hex.Length != 8)
+        {
+            return SKColors.White; // used by the PowerPoint application as the default background color
+        }
+
+        return new Color(hex).AsSkColor();
+    }
+
+    private void RenderBackground(SKCanvas canvas)
+    {
+        var slideFill = this.Fill;
+        if (slideFill is { Type: FillType.Solid, Color: not null })
+        {
+            var skColor = this.GetSkColor();
+            canvas.Clear(skColor);
+        }
+        else if (slideFill is { Type: FillType.Picture, Picture: not null })
+        {
+            var bytes = slideFill.Picture.AsByteArray();
+            using var stream = new MemoryStream(bytes);
+            using var bitmap = SKBitmap.Decode(stream);
+            var destRect = new SKRect(0, 0, canvas.DeviceClipBounds.Width, canvas.DeviceClipBounds.Height);
+            canvas.DrawBitmap(bitmap, destRect);
+        }
+        else
+        {
+            // Default to white for unsupported backgrounds.
+            canvas.Clear(SKColors.White);
+        }
+    }
+}
--- src/Slides/UserSlide.cs
+++ src/Slides/UserSlide.cs
@@ -9,14 +9,12 @@ using DocumentFormat.OpenXml.Presentation;
 using ShapeCrawler.Drawing;
 using ShapeCrawler.Presentations;
 using ShapeCrawler.Shapes;
-using ShapeCrawler.Units;
-using SkiaSharp;
 using A = DocumentFormat.OpenXml.Drawing;
 
 namespace ShapeCrawler.Slides;
 
 /// <inheritdoc/>
-internal class UserSlide(ILayoutSlide layoutSlide, UserSlideShapeCollection shapes, SlidePart slidePart) : IUserSlide
+internal abstract class UserSlide(ILayoutSlide layoutSlide, UserSlideShapeCollection shapes, SlidePart slidePart) : IUserSlide
 {
     public ILayoutSlide LayoutSlide => layoutSlide;
 
@@ -166,34 +164,11 @@ internal class UserSlide(ILayoutSlide layoutSlide, UserSlideShapeCollection shap
         where T : IShape
         => this.Shapes.Shape<T>(name);
 
-    public void SaveImageTo(string file)
-    {
-        using var fileStream = File.Create(file);
-        this.SaveImageTo(fileStream);
-    }
+    /// <inheritdoc/>
+    public abstract void SaveImageTo(string file);
 
-    public void SaveImageTo(Stream stream)
-    {
-        var presPart = this.GetSdkPresentationPart();
-        var pSlideSize = presPart.Presentation.SlideSize!;
-        var width = new Emus(pSlideSize.Cx!.Value).AsPixels();
-        var height = new Emus(pSlideSize.Cy!.Value).AsPixels();
-
-        using var surface = SKSurface.Create(new SKImageInfo((int)width, (int)height));
-        var canvas = surface.Canvas;
-
-        this.RenderBackground(canvas);
-        shapes.Render(canvas);
-
-        using var image = surface.Snapshot();
-        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-        data.SaveTo(stream);
-
-        if (stream.CanSeek)
-        {
-            stream.Position = 0;
-        }
-    }
+    /// <inheritdoc/>
+    public abstract void SaveImageTo(Stream stream);
 
     public PresentationPart GetSdkPresentationPart()
     {
@@ -408,41 +383,5 @@ internal class UserSlide(ILayoutSlide layoutSlide, UserSlideShapeCollection shap
         }
 
         return null;
-    }
-
-    private SKColor GetSkColor()
-    {
-        var hex = this.Fill.Color!.TrimStart('#');
-
-        // Validate hex length before parsing
-        if (hex.Length != 6 && hex.Length != 8)
-        {
-            return SKColors.White; // used by the PowerPoint application as the default background color
-        }
-
-        return new Color(hex).AsSkColor();
-    }
-
-    private void RenderBackground(SKCanvas canvas)
-    {
-        var slideFill = this.Fill;
-        if (slideFill is { Type: FillType.Solid, Color: not null })
-        {
-            var skColor = this.GetSkColor();
-            canvas.Clear(skColor);
-        }
-        else if (slideFill is { Type: FillType.Picture, Picture: not null })
-        {
-            var bytes = slideFill.Picture.AsByteArray();
-            using var stream = new MemoryStream(bytes);
-            using var bitmap = SKBitmap.Decode(stream);
-            var destRect = new SKRect(0, 0, canvas.DeviceClipBounds.Width, canvas.DeviceClipBounds.Height);
-            canvas.DrawBitmap(bitmap, destRect);
-        }
-        else
-        {
-            // Default to white for unsupported backgrounds
-            canvas.Clear(SKColors.White);
-        }
     }
 }
