24408fc39629475189581e027f303422237b0bad
Adam Shakhabov <theadamo86@gmail.com>
2026-01-18 16:36:32 +0100

    Format ShapeContentType.cs for improved readability

--- src/Shapes/ShapeContentType.cs
+++ src/Shapes/ShapeContentType.cs
@@ -1,4 +1,5 @@
 // ReSharper disable InconsistentNaming
+
 #pragma warning disable IDE0130
 namespace ShapeCrawler;
 
@@ -11,42 +12,42 @@ public enum ShapeContentType
     ///    Shape.
     /// </summary>
     Shape,
-    
+
     /// <summary>
     ///    Text.
     /// </summary>
     Text,
-    
+
     /// <summary>
     ///    Picture.
     /// </summary>
     Picture,
-    
+
     /// <summary>
     ///    Table.
     /// </summary>
     Table,
-    
+
     /// <summary>
     ///    Chart.
     /// </summary>
     Chart,
-    
+
     /// <summary>
     ///    SmartArt Graphic.
     /// </summary>  
     SmartArt,
-    
+
     /// <summary>
     ///     Video.
     /// </summary>
     Video,
-    
+
     /// <summary>
     ///     Audio.
     /// </summary>
     Audio,
-    
+
     /// <summary>
     ///     Group.
     /// </summary>
